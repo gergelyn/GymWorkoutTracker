508623e6efccbe751e2b73daf17bb4d77f7abc96
Nagy Gergely <gergely.nagy@erppartner.hu>
2025-08-25 18:17:53 +0200

    Exercise entity created

---
+++ src/GymWorkoutTracker.Domain/Entities/Exercise.cs
@@ -0,0 +1,8 @@
+﻿namespace GymWorkoutTracker.Domain.Entities;
+
+public class Exercise
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
