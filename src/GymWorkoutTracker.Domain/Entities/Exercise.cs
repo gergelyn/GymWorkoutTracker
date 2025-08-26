a73d3480d164a14a8b3acd57dcb83f177417f72d
Nagy Gergely <gergely.nagy@erppartner.hu>
2025-08-26 16:46:36 +0200

    Added Exercise entity to Domain layer

---
+++ src/GymWorkoutTracker.Domain/Entities/Exercise.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymWorkoutTracker.Domain.Entities;
+
+public class Exercise
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    public required string Name { get; set; }
+    
+    public string? Description { get; set; }
+}
