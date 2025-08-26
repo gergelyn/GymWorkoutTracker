16de13427dce09b3660005110bdd404002a5c14d
Nagy Gergely <gergely.nagy@erppartner.hu>
2025-08-26 16:43:00 +0200

    Added ApplicationDbContext to Infrastructure layer

---
+++ src/GymWorkoutTracker.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GymWorkoutTracker.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
